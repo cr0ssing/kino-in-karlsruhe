555d164898bc3fd4aa95643d6b6c5473c1e15aa1
Robin Lamberti <r.lamberti@civitasconnect.digital>
2024-12-20 23:26:20 +0100

    feat: add color for cinemas

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -16,6 +16,7 @@ async function main() {
         name: 'Schauburg',
         address: 'Marienstraße 16, 76137 Karlsruhe',
         website: 'www.schauburg.de',
+        color: '#d42013'
       },
     }),
     prisma.cinema.create({
@@ -23,6 +24,7 @@ async function main() {
         name: 'Kinemathek',
         address: 'Kaiserpassage 6, 76133 Karlsruhe',
         website: 'www.kinemathek-karlsruhe.de',
+        color: '#10b02b'
       },
     }),
     prisma.cinema.create({
@@ -30,6 +32,7 @@ async function main() {
         name: 'Universum',
         address: 'Kaiserstraße 152-154, 76133 Karlsruhe',
         website: 'www.kinopolis.de/ka',
+        color: '#e6ca19'
       },
     }),
     prisma.cinema.create({
@@ -37,6 +40,7 @@ async function main() {
         name: 'Filmpalast',
         address: 'Brauerstraße 40 - 76135 Karlsruhe',
         website: 'www.filmpalast.net',
+        color: '#0e41cc'
       },
     }),
   ])
