cf47bef999a897df474957d3a980c4d4f50e90f9
Robin Lamberti <r.lamberti@civitasconnect.digital>
2024-12-24 14:29:51 +0100

    chore: add license header to each file

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -1,4 +1,23 @@
 /**
+ * Copyright (C) 2024 Robin Lamberti.
+ * 
+ * This file is part of kino-in-karlsruhe.
+ * 
+ * kino-in-karlsruhe is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * kino-in-karlsruhe is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with kino-in-karlsruhe. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
  * YOU PROBABLY DON'T NEED TO EDIT THIS FILE, UNLESS:
  * 1. You want to modify request context (see Part 1).
  * 2. You want to create a new middleware or type of procedure (see Part 3).
