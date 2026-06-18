17e2171e1206f2c589c6477055493faebb839b76
Robin Lamberti <lamberti.robin@gmail.com>
2026-06-18 22:47:38 +0200

    fix: deactive crawler - RIP Universum

--- src/server/api/crawler.ts
+++ src/server/api/crawler.ts
@@ -92,7 +92,7 @@ export async function run() {
   const screenings = (await Promise.all([
     crawlSchauburg(),
     crawlKinemathek(),
-    crawlUniversum(),
+    // crawlUniversum(),
     crawlFilmpalast()
   ])).flat() as Screening[];
 
